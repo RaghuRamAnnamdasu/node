2a4a35396311765d7856d7abe34a86bee1c7c8b7
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-07-07 15:46:31 +0530

    first commit

---
+++ node_app/file.js
@@ -0,0 +1,35 @@
+const quote2 = "Live more, worry less 😀😂"
+
+// const fs = require("fs");
+
+// const[,,count]=process.argv;
+
+// for (let i=1;i<=count;i++){
+//     fs.writeFile(`./backup/text-${i}.html`, quote2,(err)=>{
+//         console.log("Completed Writing...!!")
+//     })
+// }
+
+// fs.readFile("./backup/cool.txt","utf-8",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// })
+
+// fs.appendFile("./backup/cool.txt","\n"+quote2,(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("completed appending");
+//     }
+// })
+
+
+
+// const os = require("os");
+// console.log("Free Memory", os.freemem()/1024/1024/1024);
+// console.log("Total Memory", os.totalmem()/1024/1024/1024);
+// console.log("Version", os.version());
+// console.log("Processors", os.cpus());
