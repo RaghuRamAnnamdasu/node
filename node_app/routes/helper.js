deb78ac1faeee16edb6e709b1ae5bf3a0ebf7492
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-07-13 12:53:54 +0530

    id to _id

--- node_app/routes/helper.js
+++ node_app/routes/helper.js
@@ -1,17 +1,19 @@
 import { client } from "../index.js";
+import {ObjectId} from "mongodb";
 
 export async function updateMovieById(id, data) {
-  return await client.db("node_app").collection("movies").updateOne({ id: id }, { $set: data });
+  return await client.db("node_app").collection("movies").updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
-  return await client.db("node_app").collection("movies").deleteOne({ id: id });
+  return await client.db("node_app").collection("movies").deleteOne({ _id: ObjectId(id) });
 }
 export async function createMovie(data) {
   return await client.db("node_app").collection("movies").insertMany(data);
 }
 export async function getMovieById(id) {
-  return await client.db("node_app").collection("movies").findOne({ id: id });
+  return await client.db("node_app").collection("movies").findOne({ _id: ObjectId(id) });
 }
 export async function getAllMovies(req) {
   return await client.db("node_app").collection("movies").find(req.query).toArray();
 }
+
